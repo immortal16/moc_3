82661ee3b39c0b43890d0419fd5cba524569ec47
immortal16 <zhenyav20133@gmail.com>
2021-11-25 23:17:51 +0200

    added binary search of n-th root

--- main.py
+++ main.py
@@ -48,3 +48,20 @@ def data_parser(path):
         N.append(int(config.get('RSA', f'n{i}'), 16))
 
     return C, N
+
+
+def root(x, n):
+    high = 1
+    mid = 0
+    while high ** n <= x:
+        high *= 2
+    low = high // 2
+    while low < high:
+        mid = (low + high) // 2
+        if low < mid and mid**n < x:
+            low = mid
+        elif high > mid and mid**n > x:
+            high = mid
+        else:
+            return mid
+    return mid + 1
